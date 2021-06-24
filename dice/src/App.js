0f2c5a2878aaacfaa23701a1fcb6216f26db8f83
Gautam <gomzekh@gmail.com>
2021-06-24 15:35:56 +0530

    Main App component

---
+++ dice/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import RolDice from './RollDice.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <RolDice/>
+    </div>
+  );
+}
+
+export default App;
