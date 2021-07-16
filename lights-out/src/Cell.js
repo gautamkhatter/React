5a51bc59c4f18fffea15f230008f29a0bb9396b2
Gautam <gomzekh@gmail.com>
2021-07-16 17:36:15 +0530

    Cell Component

---
+++ lights-out/src/Cell.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(evt) {
+    this.props.flipCellsAroundMe();
+  }
+
+  render() {
+    let classes = "Cell" + (this.props.isLit ? "Cell-lit" : "");
+
+    return (
+      <div className="Cell">
+        <td className={classes} onClick={this.handleClick}></td>
+      </div>  
+    )
+    
+  }   
+}
+
+export default Cell;
