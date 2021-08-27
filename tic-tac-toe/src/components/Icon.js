cba2bb1cbfddda39ed5f9d696869bfab2e9d5d22
Gautam <gomzekh@gmail.com>
2021-08-27 20:37:13 +0530

    Icon component

---
+++ tic-tac-toe/src/components/Icon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaTimes, FaPen, FaRegCircle } from "react-icons/fa";
+
+
+function Icon({ name }) {
+  switch (name) {
+    case 'circle':
+      return <FaRegCircle className="Icon" />
+    case 'cross':
+      return <FaTimes className="Icon" />
+    default:
+      return <FaPen className="Icon" />
+  }
+}
+
+
+export default Icon;
